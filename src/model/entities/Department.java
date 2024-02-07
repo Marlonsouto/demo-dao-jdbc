116e3066a9eb86ca86ffaa4ee99fcfa12f57e5f9
Marlonsouto <soutomarlon@gmail.com>
2024-02-07 10:27:01 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,16 @@
+package model.entities;
+
+public class Department {
+	private Integer id;
+	private String nome;
+	
+	public Department(Integer id, String nome) {
+		
+		this.id = id;
+		this.nome = nome;
+		
+	}
+	
+	
+
+}
---
+++ src/model/entities/Seller.java
@@ -0,0 +1,14 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Seller implements Serializable {
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	private Date birthDate;
+	
+
+}
